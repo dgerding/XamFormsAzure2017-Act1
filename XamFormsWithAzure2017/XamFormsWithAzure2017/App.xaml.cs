4452dc114b86eee9fb96aad28847b4fda2964de3
Dave Gerding <dave@versive.com>
2017-08-04 14:28:01 -0500

    This is the initial state from the solution template.

---
+++ XamFormsWithAzure2017/XamFormsWithAzure2017/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamFormsWithAzure2017
+{
+    public partial class App : Application
+    {
+        public static bool UseMockDataStore = true;
+        public static string AzureMobileAppUrl = "https://XamFormsWithAzure.azurewebsites.net";
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<AzureDataStore>();
+
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new MainPage();
+            else
+                MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
